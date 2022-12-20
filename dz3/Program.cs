e319682d9f35012229ec4b9f441468515986cd46
Michael <zamchalin@mail.ru>
2022-12-20 12:38:20 +0300

    Added dz

---
+++ dz3/Program.cs
@@ -0,0 +1,71 @@
+﻿// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+
+
+Console.Clear();
+int rows = ReadInt("Введите количество строк: ");
+int columns = ReadInt("Введите количество столбцов: ");
+int[,] matrix = new int[rows, columns];
+int[,] secondMatrix = new int[rows, columns];
+int[,] resultMatrix = new int[rows, columns];
+
+InputMatrix(matrix);
+PrintMatrix2D(matrix);
+
+Console.WriteLine();
+
+InputMatrix(secondMatrix);
+PrintMatrix2D(secondMatrix);
+
+Console.WriteLine();
+
+if (matrix.GetLength(0) != secondMatrix.GetLength(1))
+{
+    Console.WriteLine(" Нельзя перемножить ");
+    return;
+}
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < secondMatrix.GetLength(1); j++)
+    {
+        resultMatrix[i, j] = 0;
+        for (int k = 0; k < matrix.GetLength(1); k++)
+        {
+            resultMatrix[i, j] += matrix[i, k] * secondMatrix[k, j];
+        }
+    }
+}
+
+PrintMatrix2D(resultMatrix);
+
+
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+
+void PrintMatrix2D(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
